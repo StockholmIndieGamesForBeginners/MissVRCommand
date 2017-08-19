b13345addd60e5758c9e37d17c4c87b9df5c6248
IrregularProgramming <fredrik.sjoborg@gmail.com>
2017-08-19 14:46:34 +0200

    Added Controller Script

---
+++ Assets/Scripts/MissileLaunchController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissileLaunchController : MonoBehaviour {
+
+    public SteamVR_TrackedObject trackedObject;
+    public GameObject missile;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        SteamVR_Controller.Device device = SteamVR_Controller.Input((int)trackedObject.index);
+
+        if( device.GetTouch( SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Instantiate(missile, transform, true);
+        }
+    }
+}

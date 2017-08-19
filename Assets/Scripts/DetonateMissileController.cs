afd1a7528d3715084d67d1f4eb6b069c9cd7a144
IrregularProgramming <fredrik.sjoborg@gmail.com>
2017-08-19 15:25:07 +0200

    DetonateMissiles

---
+++ Assets/Scripts/DetonateMissileController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetonateMissileController : MonoBehaviour {
+    public SteamVR_TrackedObject trackedObject;
+    public GameObject missileDetonator;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        SteamVR_Controller.Device device = SteamVR_Controller.Input((int)trackedObject.index);
+
+        if (device.GetTouch(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            GameObject localMissile = Instantiate(missileDetonator, transform.position, transform.rotation);
+
+        }
+    }
+}
